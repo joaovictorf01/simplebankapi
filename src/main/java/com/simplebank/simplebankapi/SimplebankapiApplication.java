8af17700297f77f448c355ca42be14d902c041d3
João Victor <joaovictorf01@gmail.com>
2024-02-06 12:50:54 -0300

    initial commit

---
+++ src/main/java/com/simplebank/simplebankapi/SimplebankapiApplication.java
@@ -0,0 +1,13 @@
+package com.simplebank.simplebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebankapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplebank/simplebankapi/SimplebankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplebank.simplebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
